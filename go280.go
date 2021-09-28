39ac60ff9e1ae144813ce87b0586e965e0cb473c
daisuke0131 <dai.tachikoma@gmail.com>
2021-09-28 12:01:33 +0900

    2021/9/28の課題途中まで

--- go280.go
+++ go280.go
@@ -1,6 +1,7 @@
 package go280
 
 import (
+	"fmt"
 	"golang.org/x/tools/go/ssa"
 
 	"golang.org/x/tools/go/analysis"
@@ -29,16 +30,95 @@ func (*isPanic) String() string{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	reportPanic(pass)
+	reportNotRecover(pass)
+	return nil, nil
+}
+
+func reportPanic(pass *analysis.Pass){
 	s := pass.ResultOf[buildssa.Analyzer].(*buildssa.SSA)
-	positives := map[*ssa.Function]
+	positives := map[*ssa.Function][]*ssa.Function{}
 	for _, f := range s.SrcFuncs {
 		if isPanicFunc(pass,f){
 			if f.Object() != nil {
 				pass.ExportObjectFact(f.Object(), new(isPanic))
 			}
+		} else {
+			recordCallee(pass,f,positives)
 		}
 	}
-	return nil, nil
+	for k, v := range positives{
+		if pass.ImportObjectFact(k.Object(),new(isPanic)){
+			for _, v2 := range v {
+				exportFact(pass, v2, positives)
+			}
+		}
+	}
+}
+
+func reportNotRecover(pass *analysis.Pass){
+	s := pass.ResultOf[buildssa.Analyzer].(*buildssa.SSA)
+	for _, f := range s.SrcFuncs {
+		if isRecover(pass,f){
+			continue
+		}
+		for _, b := range f.Blocks {
+			for _, instr := range b.Instrs {
+				call, _ := instr.(*ssa.Call)
+				if call == nil {
+					continue
+				}
+				callee, _ := call.Common().Value.(*ssa.Function)
+				if callee == nil{
+					continue
+				}
+				if callee.Object() == nil{
+					continue
+				}
+				if pass.ImportObjectFact(callee.Object(),new(isPanic)){
+					pass.Reportf(call.Pos(),"panic")
+				}
+			}
+		}
+	}
+}
+
+func isRecover(pass *analysis.Pass,f *ssa.Function) bool{
+	for _, b := range f.Blocks {
+		for _, instr := range b.Instrs {
+			call, _ := instr.(*ssa.Defer)
+			if call == nil{
+				continue
+			}
+			callee, _ := call.Common().Value.(*ssa.Function)
+			if callee == nil{
+				continue
+			}
+			if callee.Object() == nil{
+				continue
+			}
+			for _, v := range callee.Blocks{
+				for _, instr2 := range v.Instrs {
+					call2, _ := instr2.(*ssa.Call)
+					if call2 == nil{
+						continue
+					}
+					fmt.Printf("%T\n",call2.Common().Value)
+					callee2, _ := call2.Common().Value.(*ssa.Builtin)
+					if callee2 == nil{
+						continue
+					}
+					if callee2.Object() == nil{
+						continue
+					}
+					if callee2.Name() == "recover"{
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
 }
 
 func isPanicFunc(pass *analysis.Pass,f *ssa.Function) bool{
@@ -48,7 +128,6 @@ func isPanicFunc(pass *analysis.Pass,f *ssa.Function) bool{
 			if p != nil {
 				return true
 			}
-
 			call, _ := instr.(*ssa.Call)
 			if call == nil{
 				continue
@@ -57,7 +136,6 @@ func isPanicFunc(pass *analysis.Pass,f *ssa.Function) bool{
 			if callee == nil{
 				continue
 			}
-
 			if callee.Object() == nil{
 				continue
 			}
@@ -68,3 +146,40 @@ func isPanicFunc(pass *analysis.Pass,f *ssa.Function) bool{
 	}
 	return false
 }
+
+func recordCallee(pass *analysis.Pass,f *ssa.Function, m map[*ssa.Function][]*ssa.Function){
+	if f.Object() == nil{
+		return
+	}
+	if pass.ImportObjectFact(f.Object(),new(isPanic)){
+		return
+	}
+	for _, b := range f.Blocks {
+		for _, instr := range b.Instrs {
+			call, _ := instr.(*ssa.Call)
+			if call == nil{
+				continue
+			}
+			callee, _ := call.Common().Value.(*ssa.Function)
+			if callee == nil{
+				continue
+			}
+			if callee.Object() == nil{
+				continue
+			}
+			m[callee] = append(m[callee], f)
+		}
+	}
+}
+
+func exportFact(pass *analysis.Pass,f *ssa.Function,m map[*ssa.Function][]*ssa.Function){
+	if pass.ImportObjectFact(f.Object(),new(isPanic)){
+		return
+	}
+
+	pass.ExportObjectFact(f.Object(),new(isPanic))
+	for _, v2 := range m[f] {
+		exportFact(pass,v2,m)
+	}
+}
+
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,13 +1,26 @@
 package a
 
-import "reflect"
+import (
+	"reflect"
+)
 
 
 func main() { // want main:"isPanic"
-	f()
+	f() // want "panic"
+	g()
 }
 
 func f() { // want f:"isPanic"
+	v := reflect.ValueOf(10)
+	v.SetInt(20) // want "panic"
+}
+
+func g() {
+	defer func(){
+		if p := recover(); p != nil{
+			println("recover")
+		}
+	}()
 	v := reflect.ValueOf(10)
 	v.SetInt(20)
 }
